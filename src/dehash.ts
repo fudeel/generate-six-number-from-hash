2ecbe5857c5f634ac50e323efd8bdb61a3c591b0
Fadil M <ext.Fadil.Muca@dmz.skoda-auto.cz>
2022-08-31 10:41:04 +0200

    uncommented

--- src/dehash.ts
+++ src/dehash.ts
@@ -25,11 +25,10 @@ const dehash = (hash: string): string[] => {
         const left = hash.slice(0, Math.ceil(hash.length / 2));
         const right = hash.slice(left.length);
 
-        dehash(left); // recursion
-        dehash(right); // recursion
+        dehash(left);
+        dehash(right);
     } else {
         if (isANumber(hash.charAt(0))){
-            // clean the hash from invalid data and push them into a new array
             dehashList.push(hash);
         }
     }
@@ -53,37 +52,28 @@ export const inOrderCombination = (hash: string): number[] => {
         let current: string = null;
         const len = combination.length;
         if (len < 6){
-            /* if there's space in the six positions */
             if (dehashList[i+1]) {
-                /* merge with the next valid value randomly. Sometimes picks the figure alone 1 or 2, sometimes merge
-                    it with the next one, like 12, 23
-                 */
                 const addNext = Math.random();  /* returns from 0 to 1 */
                 if (len < 5 && addNext > 0.5 && (Number(dehashList[i] + Number(dehashList[i+1])) <= 70)) {
-                    /* if there is place for normal numbers and if those numbers merged together are less or equal to 70 */
                     current = dehashList[i] + dehashList[i+1];
                     if (Number(current) !== 0 && !combination.includes(Number(current))) {
                         combination.push(Number(current));
                         i++;
                     }
                 } else if (len < 6 && addNext > 0.5 && (Number(dehashList[i] + Number(dehashList[i+1])) <= 28)) {
-                    /* if there's no space for normal numbers and it's magic number's turn */
                     current = dehashList[i] + dehashList[i+1];
                     if (Number(current) !== 0 && !combination.includes(Number(current))) {
                         combination.push(Number(current));
                         i++;
                     }
                 } else {
-                    /* if the random decided to not merge with the next one */
                     current = dehashList[i];
                 }
             } else {
-                /* if there's no next valid value in the array */
                 current = dehashList[i];
             }
 
             if (Number(current) !== 0 && !combination.includes(Number(current))) {
-                /* push the merged or alone value to the array only if it is not 0 or it does not exist already */
                 combination.push(Number(current));
             }
         } else {
